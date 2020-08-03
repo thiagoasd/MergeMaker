9dbbd8b8247c458dafe42e85f18e77da18cb5800
Thiago Teixeira <tteixeira@indracompany.com>
2020-08-03 08:44:37 -0300

    Thiago Teixeira - Ajustes spec e package

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('MergeMaker');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MergeMaker app is running!');
-  });
 });
